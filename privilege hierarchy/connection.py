f38eaeb7eb62bdce4508f6e78d4cae1ffed283bd
Antonio <adipio@kent.edu>
2021-10-27 15:04:26 -0400

    update to the code
    update to the code

---
+++ privilege hierarchy/connection.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+def fpdatabase():
+    try:
+        results = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "1234",
+        database = "fpdatabase",)
+        return results
+    except:
+        print("Something went wrong with connecting to the database. \n Try checking if the SQL server is running, and check if you have the correct password in backend/connection.py")
