1768e6c946637e9315836750bfaf34bb5d8e400c
Antonio <adipio@kent.edu>
2021-11-04 14:00:32 -0400

    adding view functions
    adding view functions.

--- privilege hierarchy/main.py
+++ privilege hierarchy/main.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def fpdatabase():
+def universitydb():
     try:
         results = mysql.connector.connect(
         host = "localhost",
@@ -15,37 +15,79 @@ def fpdatabase():
 #Adds a user to the user data base ant then returns true if they were added correctly
 def addFaculty(firstName, lastName, email, job):
     universitydb = connection.universitydb()
-    my_cursor = fpdatabase.cursor()
+    my_cursor = universitydb.cursor()
     sqlStuff = "INSERT INTO faculty (firstName, lastName, email, JOB) VALUES (%s,%s,%s,%s)"
     my_cursor.execute(sqlStuff, (firstName, lastName, email, job))
-    fpdatabase.commit()
+    universitydb.commit()
     my_cursor.close()
 
 def addCourse(className, professorName, studentList, roomNumber, classID, creditHours, roomCapacity, numStudents, syllabus):
     universitydb = connection.universitydb()
-    my_cursor = fpdatabase.cursor()
+    my_cursor = universitydb.cursor()
     sqlStuff = "INSERT INTO courses (className, professorName, studentList, roomNumber, classID, creditHours, roomCapacity, numStudents, syllabus) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     my_cursor.execute(sqlStuff, (className, professorName, studentList, roomNumber, classID, creditHours, roomCapacity, numStudents, syllabus))
-    fpdatabase.commit()
+    universitydb.commit()
     my_cursor.close()
 
 def addStudent(firstName, lastName, studentID, creditHours, year, age, Advisor):
     universitydb = connection.universitydb()
-    my_cursor = fpdatabase.cursor()
+    my_cursor = universitydb.cursor()
     sqlStuff = "INSERT INTO students (firstName, lastName, studentID, creditHours, year, age, Advisor) VALUES (%s,%s,%s,%s,%s,%s,%s)"
     my_cursor.execute(sqlStuff, (firstName, lastName, studentID, creditHours, year, age, Advisor))
-    fpdatabase.commit()
+    universitydb.commit()
     my_cursor.close()
 
-def deleteUser(admin): # what do we wanna delete by on each table?
-    fpdatabase = connection.fpdatabase()
-    my_cursor = fpdatabase.cursor()
-    sql = "DELETE FROM user WHERE userID = %s"
+def deleteFaculty(): # what do we wanna delete by on each table?
+    universitydb = connection.universitydb()
+    my_cursor = universitydb.cursor()
+    sql = "DELETE FROM faculty WHERE userID = %s"
     user = (input("Enter userId you wish to delete: "),)
     my_cursor.execute(sql, user)
-    fpdatabase.commit()
-    print("User deleted")
+    universitydb.commit()
+    print("Faculty deleted")
     my_cursor.close()
+
+def deleteCourse(): # what do we wanna delete by on each table?
+    universitydb = connection.universitydb()
+    my_cursor = universitydb.cursor()
+    sql = "DELETE FROM courses WHERE userID = %s"
+    user = (input("Enter userId you wish to delete: "),)
+    my_cursor.execute(sql, user)
+    universitydb.commit()
+    print("Course deleted")
+    my_cursor.close()
+
+def deleteStudent(): # what do we wanna delete by on each table?
+    universitydb = connection.universitydb()
+    my_cursor = universitydb.cursor()
+    sql = "DELETE FROM students WHERE userID = %s"
+    user = (input("Enter userId you wish to delete: "),)
+    my_cursor.execute(sql, user)
+    universitydb.commit()
+    print("Course deleted")
+    my_cursor.close()
+
+def viewFaculty():
+    universitydb = connection.universitydb()
+    my_cursor = universitydb.cursor()
+    my_cursor.execute("SELECT * FROM faculty")
+    results = my_cursor.fetchall()
+    return results
+
+def viewCourses():
+    universitydb = connection.universitydb()
+    my_cursor = universitydb.cursor()
+    my_cursor.execute("SELECT * FROM courses")
+    results = my_cursor.fetchall()
+    return results
+
+def viewStudents():
+    universitydb = connection.universitydb()
+    my_cursor = universitydb.cursor()
+    my_cursor.execute("SELECT * FROM students")
+    results = my_cursor.fetchall()
+    return results
+
 
 '__main__'
 
